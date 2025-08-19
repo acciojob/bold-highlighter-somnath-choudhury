e3299ccf84865ce3a4aa83dc80de3eebad86f9c7
Somnath Choudhury <102171721+somnath-choudhury@users.noreply.github.com>
2025-08-19 19:27:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
+const strongElements  = document.querySelectorAll('strong')
+
 function highlight() {
-    //Write your code here
-
-
+  strongElements.forEach((item) => {
+    item.style.color = 'rgb(0, 128, 0)';
+  })
 }
 
-
 function return_normal() {
-    //Write your code here
-
-    
+strongElements.forEach((item) => {
+  item.style.color = "rgb(0, 0, 0)";
+});
 }
